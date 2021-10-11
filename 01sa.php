37576d6668cf0172f3565ebe7096bbfcb008406b
LorraineJoyPCelespara <201980259@psu.palawan.edu.ph>
2021-10-11 20:03:14 +0800

    01sa.php done

---
+++ 01sa.php
@@ -0,0 +1,50 @@
+<?php
+
+$data = array(
+	array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+	array('Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie'),
+	array('Grass', 'Grass', 'Grass', 'Fire', 'Fire', 'Fire', 'Water', 'Water', 'Water', 'Bug'),
+	array('Poison', 'Poison', 'Poison', 'N/A', 'N/A', 'Flying', 'N/A', 'N/A', 'N/A', 'N/A'),
+	array('Overgrow', 'Overgrow', 'Overgrow', 'Blaze', 'Blaze', 'Blaze', 'Torrent', 'Torrent', 'Torrent', 'Shield Dust'),
+	array(45, 60, 80, 39, 58, 78, 44, 59, 79, 50),
+	array(49, 62, 82, 52, 64, 84, 48, 63, 83, 20),
+	array(49, 63, 83, 43, 58, 78, 65, 80, 100, 55)
+	);
+
+	echo "|id: ".$data[0][0]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][0]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][0]."&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][0]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][0]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][0]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][0]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][1]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][1]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][1]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][1]."&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][1]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][1]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][1]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][1]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][2]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][2]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][2]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][2]."&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][2]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][2]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][2]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][2]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][3]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][3]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][3]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][3]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][3]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"." |hp: ".$data[5][3]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][3]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][3]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][4]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][4]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][4]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][4]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][4]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][4]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][4]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][4]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][5]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][5]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][5]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][5]."&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][5]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][5]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][5]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][5]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][6]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][6]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][6]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][6]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][6]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][6]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][6]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][6]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][7]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][7]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][7]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][7]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][7]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][7]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][7]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][7]."&nbsp;&nbsp;&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][8]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][8]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][8]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][8]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][8]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][8]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][8]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][8]."&nbsp;| <br>";
+	echo "<br>";
+	echo "|id: ".$data[0][0]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|species: ".$data[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type1: ".$data[2][0]."&nbsp;&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|type2: ".$data[3][0]."&nbsp;&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|ability: ".$data[4][0]."&nbsp;|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|hp: ".$data[5][0]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|attack:".$data[6][9]."|"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."|defense: ".$data[7][9]."&nbsp;&nbsp;&nbsp;| <br>";
+	
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
+	echo "|Number of Elements: ".count($data)."|<br>";
+
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
+$favorite = array(9, 'Blastoise', 'Water', 'N/A', 'Torrent', 79, 83, 100);
+
+	foreach($data[1] as $datum){
+		echo $datum;
+		echo '<br>';
+	}
+?>
